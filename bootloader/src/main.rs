21cc06280282ac0a1e6f311b4c1faf0bfd09eb8b
youta ogino <dev.youta1119@gmail.com>
2018-07-28 10:05:13 +0900

    create bootloader

--- bootloader/src/main.rs
+++ bootloader/src/main.rs
@@ -1,5 +1,26 @@
-extern crate lib_x86;
+#![feature(lang_items)]
+#![feature(panic_implementation)]
+#![feature(global_asm)]
+//#![feature(asm)]
+#![no_std]
+#![no_main]
 
-fn main() {
-    lib_x86::hello();
+use core::panic::PanicInfo;
+global_asm!(include_str!("boot.S"));
+
+#[no_mangle]
+pub extern "C" fn bootmain()  {
+    return
+}
+
+#[panic_implementation]
+#[no_mangle]
+pub extern "C" fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[lang = "eh_personality"]
+#[no_mangle]
+pub extern "C" fn eh_personality() {
+    loop {}
 }
