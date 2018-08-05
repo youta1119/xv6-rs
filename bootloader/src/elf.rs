185d66ad301094d331c59c98d10e1bf0882dd7c0
youta ogino <dev.youta1119@gmail.com>
2018-08-05 11:53:50 +0900

    implements rust

--- bootloader/src/elf.rs
+++ bootloader/src/elf.rs
@@ -2,31 +2,32 @@ pub const ELF_MAGIC: u32 = 0x464C457F;  // "\x7FELF" in little endian
 
 #[repr(C)]
 pub struct ElfHeader {
-    pub e_magic: u32,
-    pub e_elf: [u8; 12],
-    pub e_type: u16,
-    pub e_machine: u16,
-    pub e_version: u32,
-    pub e_entry: u32,
-    pub e_phoff: u32,
-    pub e_shoff: u32,
-    pub e_flags: u32,
-    pub e_ehsize: u16,
-    pub e_phentsize: u16,
-    pub e_phnum: u16,
-    pub e_shentsize: u16,
-    pub e_shnum: u16,
-    pub e_shstrndx: u16,
+    pub magic: u32,
+    pub elf: [u8; 12],
+    pub typ: u16,
+    pub machine: u16,
+    pub version: u32,
+    pub entry: u32,
+    pub phoff: u32, // physical header offset
+    pub shoff: u32,
+    pub flags: u32,
+    pub ehsize: u16,
+    pub phentsize: u16,
+    pub phnum: u16,
+    pub shntsize: u16,
+    pub shnum: u16,
+    pub shstrndx: u16,
 }
 
 #[repr(C)]
 pub struct ProgramHeader {
-    pub p_type: u32,
-    pub p_offset: u32,
-    pub p_va: u32,
-    pub p_pa: u32,
-    pub p_filesz: u32,
-    pub p_memsz: u32,
-    pub p_flags: u32,
-    pub p_align: u32,
+    pub typ: u32,
+    pub off: u32,
+    pub vaddr: u32,
+    pub paddr: u32,
+    pub filesz: u32,
+    pub memsz: u32,
+    pub flags: u32,
+    pub align: u32,
 }
+
--- bootloader/src/main.rs
+++ bootloader/src/main.rs
@@ -7,93 +7,81 @@
 
 extern crate x86;
 
-//mod elf;
-
-//use x86::instruction;
+mod elf;
+use elf::{ElfHeader, ProgramHeader, ELF_MAGIC};
+use x86::instruction::{inb, insl, outb};
 use core::panic::PanicInfo;
 
 global_asm!(include_str!("boot.S"));
 
+const SECTOR_SIZE: u32 = 512;
 
-//const SECTOR_SIZE: usize = 512;
-static HELLO: &[u8] = b"Hello World!";
 #[no_mangle]
-pub extern "C" fn bootmain() {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-
-    loop {}
-    //let elf = 0x10000 as *const elf::ElfHeader;// scratch space
-    //readseg(elf as u32, SECTOR_SIZE * 8, 0);
-    /*if (*elf).e_magic != elf::ELF_MAGIC {
+pub unsafe extern "C" fn bootmain() {
+    let elf_header = 0x10000 as *const ElfHeader;// scratch space
+    read_segment(elf_header as u32, SECTOR_SIZE * 8, 0);
+    if (*elf_header).magic != ELF_MAGIC {
+        //outw(0x8A00, 0x8A00);
+        //outw(0x8A00, 0x8E00);
         return;
-    }*/
-   /* // load each program segment (ignores ph flags)
-    let mut ph = (ELFHDR as *mut u8).offset(ELFHDR.e_phoff) as *mut Proghdr;
-    let mut eph = ph.offset(ELFHDR.e_phnum);
-    while ph < eph
-        // p_pa is the load address of this segment (as well
-        // as the physical address)
-        {
-            read_segment(ph.p_pa, ph.p_memsz, ph.p_offset);
-            ph = ph.offset(1)
-        }
+    }
+    // load each program segment (ignores ph flags)
+    let mut ph = ((elf_header as *const u8).offset((*elf_header).phoff as isize)) as *const ProgramHeader;
+    let eph = ph.offset((*elf_header).phnum as isize);
+    while ph < eph {
+        read_segment((*ph).paddr, (*ph).memsz, (*ph).off);
+        ph = ((ph as u32) + 32) as *const ProgramHeader; //ph.offset(1);
+    }
     // call the entry point from the ELF header
     // note: does not return!
-    loop {}*/
+    let entry: extern "C" fn() -> ! = core::mem::transmute((*elf_header).entry);
+    entry();
 }
 
-/*
 // Read 'count' bytes at 'offset' from kernel into physical address 'pa'.
 // Might copy more than asked
-#[no_mangle]
-pub unsafe extern "C" fn read_segment(pa: u32, count: u32, offset: u32) {
-    let end_pa = pa + count;
+#[inline(never)]
+unsafe fn read_segment(pa: u32, count: u32, offset: u32) {
+    let epa = pa + count;
     // round down to sector boundary
-    let begin_pa = pa & !(SECTOR_SIZE - 1);
+    let mut bpa = pa & !(SECTOR_SIZE - 1);
 
     // translate from bytes to sectors, and kernel starts at sector 1
     let mut offset = (offset / SECTOR_SIZE) + 1;
     // If this is too slow, we could read lots of sectors at a time.
     // We'd write more to memory than asked, but it doesn't matter --
     // we load in increasing order.
-    let iter = begin_pa..end_pa;
-    for pa in (begin_pa..end_pa).step_by(SECTOR_SIZE) {
+    while bpa < epa {
         // Since we haven't enabled paging yet and we're using
         // an identity segment mapping (see boot.S), we can
         // use physical addresses directly.  This won't be the
         // case once JOS enables the MMU.
-        read_sector(pa , offset);
+        read_sector(bpa , offset);
         offset += 1;
-    };
+        bpa += SECTOR_SIZE;
+    }
 }
 
 unsafe fn wait_disk() {
     // wait for disk reaady
-   // while (inb(0x1f7) & 0xc0) != 0x40 {};
+    while (inb(0x1F7) & 0xC0) != 0x40 {};
 }
 
-unsafe fn read_sector(mut dst: u32, mut offset: u32) {
-    /*// wait for disk to be ready
+unsafe fn read_sector(dst: u32, offset: u32) {
+    // wait for disk to be ready
     wait_disk();
-    outb(498, 1); // count = 1
-    outb(499, offset as u8);
-    outb(500, (offset >> 8) as u8);
-    outb(501, (offset >> 16) as u8);
-    outb(502, ((offset >> 24) | 0xe0) as u8);
-    outb(503, 0x20); // cmd 0x20 - read sectors
+    outb(0x1F2, 1); // count = 1
+    outb(0x1F3, offset as u8);
+    outb(0x1F4, (offset >> 8) as u8);
+    outb(0x1F5, (offset >> 16) as u8);
+    outb(0x1F6, ((offset >> 24) | 0xE0) as u8);
+    outb(0x1F7, 0x20); // cmd 0x20 - read sectors
     // wait for disk to be ready
     wait_disk();
     // read a sector
-    insl(496, dst, SECTOR_SIZE / 4);*/
+    insl(0x1F0, dst, SECTOR_SIZE / 4);
 }
-*/
+
 #[panic_implementation]
 #[no_mangle]
 pub extern "C" fn panic(_info: &PanicInfo) -> ! {
