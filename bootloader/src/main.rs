fb07e77cc21e759a9be44bd2c31a5a78c4a330cc
youta ogino <dev.youta1119@gmail.com>
2018-12-09 07:02:44 +0900

    setup docker file

--- bootloader/src/main.rs
+++ bootloader/src/main.rs
@@ -1,7 +1,6 @@
 #![no_std]
 #![no_main]
 #![feature(lang_items)]
-#![feature(panic_handler)]
 #![feature(global_asm)]
 #![feature(asm)]
 
--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,28 +1,21 @@
 #![no_std]
-//#![no_main]
+#![no_main]
 #![feature(lang_items)]
-#![feature(panic_handler)]
-#![feature(global_asm)]
 #![feature(asm)]
 extern crate x86;
-
 use core::panic::PanicInfo;
 mod io;
 use io::uart::Uart;
+
 //#[macro_use]
 //mod console;
-global_asm!(include_str!("entry.S"));
+//global_asm!(include_str!("entry.S"));
 static HELLO: &[u8] = b"\nHello World!!!!!";
 
 #[no_mangle]
 pub extern "C" fn i386_init() {
-    //let vga_buffer = (0xf0000000 + 0xb8000) as *mut u8;
     let uart: Uart = Uart::new().ok().expect("uart error");
-    for (i, &byte) in HELLO.iter().enumerate() {
-        /*unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }*/
+    for &byte in HELLO.iter() {
         uart.write_byte(byte);
     }
     loop {}
